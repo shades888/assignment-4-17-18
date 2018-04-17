6079742e21283d91f4b9027408a2b24da7023def
shades888 <jamaribroussard@gmail.com>
2018-04-17 13:09:48 -0500

    Second commit

---
+++ Name.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class Name {
+
+	public static void main(String[] args) {
+		String name = "Jamari Ferrari ";
+		String age = "100 years old";
+		String address = "777 Lucky Drive, San Andreas, CA 66441";
+		
+		System.out.print(name + "\n" + age + "\n" + address);
+	}
+
+}
